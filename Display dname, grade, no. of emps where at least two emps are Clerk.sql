c049a20c30267fc093b75aad49750de4c5995599
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-04-01 17:44:34 +0530

    Create Display dname, grade, no. of emps where at least two emps are Clerk

---
+++ Display dname, grade, no. of emps where at least two emps are Clerk.sql
@@ -0,0 +1,9 @@
+-- Display dname, grade, no. of emps where at least two emps are Clerk.
+
+
+SELECT D.DNAME, S.GRADE, COUNT(*)
+FROM DEPT D, SALGRADE S, EMP E
+WHERE E.DEPTNO = D.DEPTNO AND E.JOB = 'CLERK
+AND E.SAL BETWEEN S.LOSAL AND S.HISAL
+GROUP BY D.DNAME, S.GRADE
+HAVING COUNT(*) >= 2;
