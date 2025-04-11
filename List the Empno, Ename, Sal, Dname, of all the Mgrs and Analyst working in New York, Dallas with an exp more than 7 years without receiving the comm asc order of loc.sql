144cb091c6c45709a5f29b5b2fc6201be68a136a
AJAY R <63496620+ajayravi238@users.noreply.github.com>
2025-04-11 23:09:19 +0530

    Update List the Empno, Ename, Sal, Dname, of all the Mgrs and Analyst working in New York, Dallas with an exp more than 7 years without receiving the comm asc order of loc

--- List the Empno, Ename, Sal, Dname, of all the Mgrs and Analyst working in New York, Dallas with an exp more than 7 years without receiving the comm asc order of loc.sql
+++ List the Empno, Ename, Sal, Dname, of all the Mgrs and Analyst working in New York, Dallas with an exp more than 7 years without receiving the comm asc order of loc.sql
@@ -1,6 +1,5 @@
 -- List the Empno, Ename, Sal, Dname, of all the Mgrs and Analyst working in New York, Dallas with an exp more than 7 years without receiving the comm asc order of loc.
 
-
 SELECT E.EMPNO, E.ENAME, E.SAL, D.DNAME
 FROM EMP E, DEPT D
 WHERE D.LOC IN('NEW YORK', 'DALLAS') AND E.DEPTNO = D.DEPTNO AND E.DEPTNO IN(
